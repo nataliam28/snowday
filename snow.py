81bae4d944a472de3eb7a49c927e79708595ff8c
Natalia Matuk <nataliam@bu.edu>
2021-02-01 21:27:34 -0500

    adding snow.py file with hello world function

--- snow.py
+++ snow.py
@@ -1,6 +1,12 @@
 def hello_world(snow):
     snowday =1
-    if snow == 0:
+    if snow//2 == 0:
         print("yay!")
         return 0
+    print("sad :(")
     return 1
+
+if __name__ == "__main__":
+    hello_world(0)
+    hello_world(2)
+    hello_world(3)
